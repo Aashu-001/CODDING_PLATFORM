6ae204097973e968913a14f98eee503c595a2bb5
Aashu-001 <aashusharmaa639827@gmail.com>
2026-01-31 14:52:15 +0530

    Update Redis client configuration for new host and port

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-14269.c241.us-east-1-4.ec2.cloud.redislabs.com',
-        port: 14269
+         host: 'redis-14633.crce276.ap-south-1-3.ec2.cloud.redislabs.com',
+        port: 14633
     }
 });
 
