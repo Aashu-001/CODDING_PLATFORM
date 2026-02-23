8ffc10b58cf8d5dd205cbcd6f59ad3f2561845e0
Aashu-001 <aashusharmaa639827@gmail.com>
2026-02-23 21:29:59 +0530

    Refactor validation logic and error messages

--- backend/src/utils/validator.js
+++ backend/src/utils/validator.js
@@ -1,21 +1,21 @@
-const validator =require("validator");
+const validator = require("validator");
 
 // req.body 
 
-const validate = (data)=>{
-   
-    const mandatoryField = ['firstName',"emailId",'password'];
+const validate = (data) => {
 
-    const IsAllowed = mandatoryField.every((k)=> Object.keys(data).includes(k));
+    const mandatoryField = ['firstName', "emailId", 'password'];
 
-    if(!IsAllowed)
+    const IsAllowed = mandatoryField.every((k) => Object.keys(data).includes(k));
+
+    if (!IsAllowed)
         throw new Error("Some Field Missing");
 
-    if(!validator.isEmail(data.emailId))
+    if (!validator.isEmail(data.emailId))
         throw new Error("Invalid Email");
 
-    if(!validator.isStrongPassword(data.password))
-        throw new Error("Week Password");
+    if (!validator.isStrongPassword(data.password, { minLength: 8, minLowercase: 0, minUppercase: 0, minNumbers: 1, minSymbols: 0 }))
+        throw new Error("Password must be at least 8 characters long and include at least one number.");
 }
 
 module.exports = validate;
