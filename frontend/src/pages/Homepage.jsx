dc5f788a5bb522fe271dbb48ef5ab6d05e80987c
Aashu-001 <aashusharmaa639827@gmail.com>
2026-03-08 15:20:36 +0530

    Rename 'LeetCode' link to 'CodeJudge'

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -56,7 +56,7 @@ function Homepage() {
       {/* Navigation Bar */}
       <nav className="navbar bg-base-100 shadow-lg px-4">
         <div className="flex-1">
-          <NavLink to="/problems" className="btn btn-ghost text-xl font-bold">LeetCode</NavLink>
+          <NavLink to="/problems" className="btn btn-ghost text-xl font-bold">CodeJudge</NavLink>
         </div>
         <div className="flex-none gap-4">
           <div className="dropdown dropdown-end">
